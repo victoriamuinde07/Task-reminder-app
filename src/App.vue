cb326ddde53ee6b3cdfb271fed5792ba3fb1fbaf
victoriamuinde07 <victoriamuinde07@gmail.com>
2025-05-09 08:29:26 +0300

    added some components

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,21 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1> Task Manager </h1>
+    <input v-model="newTask" @keyup.enter="addTask" placeholder="Add a task" />
+    <ul>
+      <li v-for ="(task,index) in filteredTasks" :key="index">
+        <input type="checkbox" v-model="task.completed" />
+        <span :class="{completed: task.completed}">{{ task.text }}</span>
+        <button @click="deleteTask(index)">Delete</button>
+        <button @click="editTask(index)">Edit</button>
+      </li>
+    </ul>
+    <div>
+      <button @click="filter = 'all'">All</button>
+      <button @click="filter = 'active'">Active</button>
+      <button @click="filter = 'completed '">Completed</button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,33 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const { createApp,ref,computed } = Vue; 
+createApp({
+    setup() {
+        const newTask = ref("");
+        const tasks = ref([]);
+        const filter = ref("all");
+        const addTask = () => {
+            if(newTask.value.trim()) {
+                tasks.value.push({text:newTask.value, completed:false});
+                newTask.value = "";
+            };
+            const deleteTask = (index) => {
+                tasks.value.splice(index,1);
+            };
+            const filteredTasks = computed(() => {
+                if(filter.value === "active") return tasks.value.filter(t => !t.completed);
+                if(filter.value === "completed") return tasks.value.filter(t => t.completed);
+                return tasks.value;
+            });
+            return {
+                newTask,
+                tasks,
+                filter,
+                addTask,
+                deleteTask,
+                filteredTasks
+            };
+        }       
+    }
+}).mount("#app");
